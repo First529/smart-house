b72cc9453dd07cb787be292e0a13d189da49da33
First529 <first_dejabhol@hotmail.com>
2018-07-20 13:11:46 +0700

    finished

---
+++ app.js
@@ -0,0 +1,25 @@
+$(function() {
+    $("#button1") .mouseover(function () {
+        this.src= "./door2.png"
+     }).mouseout(function () {
+         this.src= "./closeddoor.png"
+     });
+
+     $("#button2") .mouseover(function () {
+        this.src= "./on.png"
+     }).mouseout(function () {
+         this.src= "./off.png"
+     });
+
+     $("#button3") .mouseover(function () {
+        this.src= "./high.png"
+     }).mouseout(function () {
+         this.src= "./low.png"
+     });
+
+     $("#button4") .mouseover(function () {
+        this.src= "./buzz.png"
+     }).mouseout(function () {
+         this.src= "./bell.png"
+     });
+});
